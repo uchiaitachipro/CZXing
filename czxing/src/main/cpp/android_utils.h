ac3fae14c890bd76318b8273c9eede46a121f9ce
chenyongkang <chenyongkang@theduapp.com>
2019-09-27 18:34:10 +0800

    添加 Mat 转 Bitmap工具类

---
+++ czxing/src/main/cpp/android_utils.h
@@ -0,0 +1,18 @@
+//
+// Created by uchiachen on 2019-09-27.
+//
+
+#ifndef CZXING_ANDROID_UTILS_H
+#define CZXING_ANDROID_UTILS_H
+
+
+#include <android/bitmap.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+void bitmap_to_mat(JNIEnv *env, jobject &srcBitmap, Mat &srcMat);
+
+void mat_to_bitmap(JNIEnv *env, Mat &srcMat, jobject &dstBitmap);
+
+#endif //CZXING_ANDROID_UTILS_H
